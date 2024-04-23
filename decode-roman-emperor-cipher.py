e4283a230ba9702e5f265b910d699e65d6f054b9
mhacxermood914 <soultonew2@gmail.com>
2024-04-23 15:30:33 +0100

    feat: roman emperor decipher challenge of crypto hack resolved

---
+++ decode-roman-emperor-cipher.py
@@ -0,0 +1,26 @@
+
+# Crypto Hack  Registration Challenges
+# Make use of roman emperor decipher
+
+cipher = 'LJAKXW EJURM HXDCQ MXEN'
+
+# def roman_emperor_cipher(cipher, shift):
+#     crypted=""
+#     for i in cipher:
+#         val = (ord(i) + shift + 25)%90 + 65
+#         while val > 90: val = val%90 + 64
+#         crypted+=chr(val)
+        
+#     return crypted
+
+def roman_emperor_decipher(cipher, shift):
+    decrypted=""
+    for i in cipher:
+        val = (ord(i) - shift + 25)%90 + 65
+        while val > 90: val = val%90 + 26
+        decrypted+=chr(val)
+    return decrypted
+
+for i in cipher.split():
+    print(i,roman_emperor_decipher(i, 9), ' ')
+
