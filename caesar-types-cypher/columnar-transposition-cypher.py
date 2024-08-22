e4b58ed08af2fe6e2dce766b50b7587f6fe6fb69
mhacxermood914 <soultonew2@gmail.com>
2024-08-22 11:49:56 +0100

    feat: caesar-types-cypher encryption algorithm init

--- caesar-types-cypher/columnar-transposition-cypher.py
+++ caesar-types-cypher/columnar-transposition-cypher.py
@@ -2,10 +2,15 @@ import pyperclip
 
 # Encryption algorithm!!
 def encrypt(key, message):
-    cypherText = [''] * 8
+    if key is isinstance(int):
+        pass
+    else:
+        key = int(key)
+
+    cypherText = [''] * key
+
     for column in range(key):
         currentIndex = column
-
         while currentIndex < len(message):
             cypherText[column] += message[currentIndex]
-            currentIndex+=key 
+            currentIndex+=key
