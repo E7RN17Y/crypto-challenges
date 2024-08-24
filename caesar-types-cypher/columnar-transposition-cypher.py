aa4ed5eb1f7e44dfec5686fb1f0667bf45ddcfa3
mhacxermood914 <soultonew2@gmail.com>
2024-08-24 13:47:04 +0100

    feat: decryption algoritm

--- caesar-types-cypher/columnar-transposition-cypher.py
+++ caesar-types-cypher/columnar-transposition-cypher.py
@@ -2,6 +2,7 @@ import pyperclip
 
 # Encryption algorithm!!
 def encrypt(key, message):
+    print('message length', len(message))
     if isinstance(key,int):
         pass
     else:
@@ -15,7 +16,33 @@ def encrypt(key, message):
             cipherText[column] += message[currentIndex]
             currentIndex+=key
     return ''.join(cipherText)
+    
 
+def decryption(key, message):
+    total_length = len(message)
+    pre_last_parts = key - (round(len(message) / key) * key - len(message))
+    print("pre",pre_last_parts)
+    each_parts = round(len(message) / key)
+    result = ['']*pre_last_parts
+    start = 0
+    for i in range(pre_last_parts):
+        result[i]=message[start:start+each_parts]
+        start += each_parts
 
+    
+
+    plaintext=['']*each_parts
+
+    for o in range(each_parts):
+        for j in range(key):
+            try:
+                plaintext[o]+=result[j][o]
+            except: pass
+            
+
+    return ''.join(plaintext)
+ 
 if __name__ == '__main__':
-    print(encrypt(8,'Common sense is not so common.'))
+    print(encrypt(8,'Hello world...da'))
+    # print("____decrypt____")
+    print(decryption(8,encrypt(8,'Hello world...da')))
