034f33d7334b18d2f08fd406c183e3d3db6855ff
mhacxermood914 <soultonew2@gmail.com>
2024-08-25 08:46:29 +0100

    fux:bug

--- caesar-types-cypher/columnar-transposition-cypher.py
+++ caesar-types-cypher/columnar-transposition-cypher.py
@@ -28,11 +28,9 @@ def decryption(key, message):
     # re-rarrange messages
     for i in range(pre_last_parts):
         result[i]=message[start:start+each_parts]
-        print(result)
         start += each_parts
     if pre_last_parts != key:
         length = round((len(message) - start)/(key - pre_last_parts))
-        print(length)
         for j in range((key - pre_last_parts)):
             result.append(message[start:start+length])
             start+=length
@@ -49,6 +47,6 @@ def decryption(key, message):
     return ''.join(plaintext)
  
 if __name__ == '__main__':
-    print(encrypt(8,'Hello world...damn'))
+    print("Encrypted value:",encrypt(8,'Hello world...damn'))
     # print("____decrypt____")
-    print(decryption(8,encrypt(8,'Hello world...damn')))
+    print("Decrypted Value:",decryption(8,encrypt(8,'Hello world...damn')))
