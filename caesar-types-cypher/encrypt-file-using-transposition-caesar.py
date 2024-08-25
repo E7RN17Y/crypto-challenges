28d69efea930396b5ef08cc110d891630a89e614
mhacxermood914 <soultonew2@gmail.com>
2024-08-25 10:34:42 +0100

    feat: rename file and create file encryption program

--- caesar-types-cypher/encrypt-file-using-transposition-caesar.py
+++ caesar-types-cypher/encrypt-file-using-transposition-caesar.py
@@ -1,7 +1,28 @@
 from columnartranspositioncypher import encrypt, decryption
-
+import os, sys
 
 def main():
-    fileInputName= 'frankenstein.txt'
+    mode = 'decrypt'
+
+    fileInputName= 'frankenstein.txt' if mode == 'encrypt' else 'encrypted-outputfile.txt'
+
+    if not os.path.exists(fileInputName):
+        print('File doesn\'t exist')
+        sys.exit()
+
+    file = open(fileInputName, 'r')
+    content = file.read()
+    file.close()
+    if mode ==  'encrypt':
+        encrypted = encrypt(10, content)
+        outputFile = open('encrypted-outputfile.txt','w')
+        outputFile.write(encrypted)
+    else:
+        decrypted= decryption(10,content)
+        outputFile = open('decryption-outputfile.txt','w')
+        outputFile.write(decrypted)
+
+
+
 if __name__ == '__main__':
     main()
